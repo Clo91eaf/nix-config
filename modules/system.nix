0d40872f8f2da367b978f9ed74aebcb2cf32c0b5
Clo91eaf <clo91eaf@qq.com>
2025-10-09 23:32:08 +0800

    [system] do not gc system generations

--- modules/system.nix
+++ modules/system.nix
@@ -40,15 +40,16 @@
       "https://cache.nixos.org"
     ];
 
-    trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+    trusted-public-keys = [ "cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+    trusted-users = [ "root" "Clo91eaf" ];
     builders-use-substitutes = true;
   };
 
   # do garbage collection weekly to keep disk usage low
   nix.gc = {
     automatic = lib.mkDefault true;
-    dates = lib.mkDefault "weekly";
-    options = lib.mkDefault "--delete-older-than 7d";
+    # do not gc generations
+    options = lib.mkDefault "";
   };
 
   # Allow unfree packages
