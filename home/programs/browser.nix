b19280260294f54d301c67c3ccc317bfa155d064
Clo91eaf <clo91eaf@qq.com>
2025-11-21 13:48:04 +0800

    [programs] add zen-browser but disable for now

--- home/programs/browser.nix
+++ home/programs/browser.nix
@@ -1,6 +1,14 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
+  imports = [
+    inputs.zen-browser.homeModules.beta
+    # or inputs.zen-browser.homeModules.twilight
+    # or inputs.zen-browser.homeModules.twilight-official
+  ];
+
   home.packages = with pkgs; [
     firefox-bin
   ];
+
+  programs.zen-browser.enable = false;
 }
