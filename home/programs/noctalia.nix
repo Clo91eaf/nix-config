0f7aa18a3db72d291cbd8cbcafa97d74b9dc934d
Clo91eaf <clo91eaf@qq.com>
2025-11-26 14:02:12 +0800

    [programs] fix noctalia-shell dock settings and restructure widget configuration

--- home/programs/noctalia.nix
+++ home/programs/noctalia.nix
@@ -6,30 +6,39 @@
   ];
 
   # configure options
+  # https://github.com/noctalia-dev/noctalia-shell/blob/main/Assets/settings-default.json
+  # All configurable options can be found in the link above and wrong configurations will be ignored
+  # So be careful with the option that is legally configurable
   programs.noctalia-shell = {
     enable = true;
     settings = {
-      dock.enable = false;
-      bar.widgets.right = [
-        {
-          id = "Taskbar";
-        }
-        {
-          id = "Tray";
-        }
-        {
-          id = "NotificationHistory";
-        }
-        {
-          id = "Volume";
-        }
-        {
-          id = "Microphone";
-        }
-        {
-          id = "Clock";
-        }
-      ];
+      dock.enabled = false; # disable dock, be careful 'enabled' but not 'enable'
+      bar.widgets = {
+        left = [
+          { id = "ControlCenter"; }
+          {
+            id = "SystemMonitor";
+            showCpuUsage = true;
+            showCpuTemp = true;
+            showMemoryUsage = true;
+            showMemoryAsPercent = true;
+            showNetworkStats = true;
+          }
+          { id = "ActiveWindow"; }
+          { id = "MediaMini"; }
+        ];
+        center = [
+          { id = "Workspace"; }
+        ];
+        right = [
+          { id = "Taskbar"; }
+          { id = "Tray"; }
+          { id = "NotificationHistory"; }
+          { id = "Volume"; }
+          { id = "Microphone"; }
+          { id = "Clock"; }
+        ];
+      };
       wallpaper = {
         randomEnabled = true;
         overviewEnabled = true;
