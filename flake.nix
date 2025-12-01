8551e09b2eeff2827520855ffc181f490f24a05c
Clo91eaf <clo91eaf@qq.com>
2025-12-01 20:13:47 +0800

    nix flake update

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
