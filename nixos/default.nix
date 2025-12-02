4506dfaa544c96597616d686d6703877ce807e6d
Clo91eaf <clo91eaf@qq.com>
2025-12-02 15:37:29 +0800

    [nixos] remove useless local host entry for QQ auto-update blocking in networking configuration

--- nixos/default.nix
+++ nixos/default.nix
@@ -22,10 +22,6 @@
   networking = {
     networkmanager.enable = true;
     firewall.enable = false;
-    hosts."127.0.0.1" = [
-      # Block this domain to prevent QQ from auto-updating.
-      "qqpatch.gtimg.cn"
-    ];
   };
 
   environment = {
