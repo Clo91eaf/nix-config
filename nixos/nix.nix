d9905ae7b93d38db7398a7a44a1e0cfc14fa95ba
Clo91eaf <clo91eaf@qq.com>
2025-12-03 20:06:01 +0800

    [nixos] reorder cache server entries and adjust trusted keys for clarity

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -17,13 +17,13 @@
       # "https://mirrors.ustc.edu.cn/nix-channels/store"
 
       "https://cache.nixos.org"
+
+      # nix community's cache server
+      "https://nix-community.cachix.org/"
     ];
 
-    trusted-public-keys = [ "cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
-    trusted-users = [
-      "root"
-      username
-    ];
+    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+    trusted-users = [ username ];
     builders-use-substitutes = true;
   };
 
