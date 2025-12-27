8575f036a7e3aa5b55a012fabc0b7aa6e1a650fc
Clo91eaf <clo91eaf@qq.com>
2025-12-27 16:55:07 +0800

    [home/misc] add swayimg desktop entry and MIME type associations

---
+++ home/misc/mime.nix
@@ -0,0 +1,27 @@
+{ pkgs, ... }:
+{
+  xdg.desktopEntries.swayimg = {
+    name = "Swayimg";
+    exec = "${pkgs.swayimg}/bin/swayimg";
+  };
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      # swayimg
+      "image/png" = "swayimg.desktop";
+      "image/jpeg" = "swayimg.desktop";
+      "image/jpg" = "swayimg.desktop";
+      "image/gif" = "swayimg.desktop";
+      "image/bmp" = "swayimg.desktop";
+      "image/tiff" = "swayimg.desktop";
+      "image/webp" = "swayimg.desktop";
+
+      # firefox
+      "text/html" = "firefox.desktop";
+      "application/pdf" = "firefox.desktop";
+
+      # foot
+      "inode/directory" = "foot.desktop";
+    };
+  };
+}
