5ed5316fbf5d1f8859739abeb0f7db86302d36fc
Clo91eaf <clo91eaf@qq.com>
2026-06-02 21:04:38 +0800

    [misc] set google as default browser

--- home/misc/mime.nix
+++ home/misc/mime.nix
@@ -23,6 +23,10 @@
       # google-chrome
       "text/html" = "google-chrome.desktop";
       "application/pdf" = "google-chrome.desktop";
+      "x-scheme-handler/http" = "google-chrome.desktop";
+      "x-scheme-handler/https" = "google-chrome.desktop";
+      "x-scheme-handler/about" = "google-chrome.desktop";
+      "x-scheme-handler/unknown" = "google-chrome.desktop";
 
       # foot
       "inode/directory" = "foot.desktop";
